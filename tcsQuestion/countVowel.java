2acabb347aa8fcc776dbf53e37827bf2e613cce4
bsit76GitHub <brajendrasharmait76@gmail.com>
2025-07-29 07:48:14 +0530

    adding countVowel and removeVowel file

---
+++ tcsQuestion/countVowel.java
@@ -0,0 +1,27 @@
+package tcsQuestion;
+
+import java.util.Scanner;
+
+public class countVowel {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        int a = 0, e = 0, i = 0, o = 0, u = 0;
+        for (int j = 0; j < str.length(); j++) {
+            if (str.charAt(j) == 'a' || str.charAt(j) == 'A') {
+                a++;
+            } else if (str.charAt(j) == 'e' || str.charAt(j) == 'E') {
+                e++;
+            } else if (str.charAt(j) == 'i' || str.charAt(j) == 'I') {
+                i++;
+            } else if (str.charAt(j) == 'o' || str.charAt(j) == 'O') {
+                o++;
+            } else if (str.charAt(j) == 'u' || str.charAt(j) == 'U') {
+                u++;
+            }
+        }
+        System.out.println("a" + a + " " + "e" + e + " " + "i" + i + " " + "o" + o + " " + "u" + u);
+    }
+
+}
--- tcsQuestion/removeVowels.java
+++ tcsQuestion/removeVowels.java
@@ -6,21 +6,31 @@ public class removeVowels {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.next();
+
+        // method : 1 using charAt() function of string
+        for (int i = 0; i < str.length(); i++) {
+            if (!(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u' || str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I' || str.charAt(i) == 'O' || str.charAt(i) == 'U')) {
+                System.out.print(str.charAt(i));
+            }
+        }
+
+        // using toCharArray :
         char[] c = str.toCharArray();
-        // method: 1 
+        // method: 2 
         // for (int i = 0; i < c.length; i++) {
         //     if (!(c[i] == 'a' || c[i] == 'e' || c[i] == 'i' || c[i] == 'o' || c[i] == 'u' || c[i] == 'A' || c[i] == 'E' || c[i] == 'I' || c[i] == 'O' || c[i] == 'U')) {
         //         System.out.print(c[i]);
         //     }
         // }
 
-        // Method: 2
-        for (int i = 0; i < c.length; i++) {
-            String ch = String.valueOf(c[i]);
-            if (!(ch.equalsIgnoreCase("a") || ch.equalsIgnoreCase("e") || ch.equalsIgnoreCase("i") || ch.equalsIgnoreCase("o") || ch.equalsIgnoreCase("u"))) {
-                System.out.print(c[i]);
-            }
-        }
+        // Method: 3
+        // for (int i = 0; i < c.length; i++) {
+        //     String ch = String.valueOf(c[i]);
+        //     if (!(ch.equalsIgnoreCase("a") || ch.equalsIgnoreCase("e") || ch.equalsIgnoreCase("i") || ch.equalsIgnoreCase("o") || ch.equalsIgnoreCase("u"))) {
+        //         System.out.print(c[i]);
+        //     }
+        // }
+        
         scanner.close();
     }
 }
