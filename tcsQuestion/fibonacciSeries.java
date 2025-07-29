de234afac60000b31dc839286d5041e56b50db8a
bsit76GitHub <brajendrasharmait76@gmail.com>
2025-07-30 03:56:28 +0530

    updaing fibonaccaiSeries.java

--- tcsQuestion/fibonacciSeries.java
+++ tcsQuestion/fibonacciSeries.java
@@ -1,21 +1,31 @@
 package tcsQuestion;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class fibonacciSeries {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        if (n < 0) {
+        if (n < 1) {
             System.out.println("Invalid input");
         }
         int x = 0, y = 1, nextTerm;
-        System.out.print(x + " " + y);
-        for (int i = 3; i <= n; i++) {
-            nextTerm = x + y;
-            x = y;
-            y = nextTerm;
-            System.out.print(" "+nextTerm);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) {
+                list.add(0);
+            } else if (i == 2) {
+                list.add(1);
+            } else {
+                nextTerm = x + y;
+                list.add(nextTerm);
+                x = y;
+                y = nextTerm;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(list.get(i) + " ");
         }
         scanner.close();
     }
