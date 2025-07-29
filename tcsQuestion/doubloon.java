e9855b78a850a7a3297919288e9865145ade6ae9
bsit76GitHub <brajendrasharmait76@gmail.com>
2025-07-29 18:43:19 +0530

    adding doubloon file

---
+++ tcsQuestion/doubloon.java
@@ -0,0 +1,39 @@
+package tcsQuestion;
+
+import java.util.Scanner;
+
+public class doubloon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = scanner.next();
+        }
+        scanner.close();
+        for (String string : str) {
+            char[] c = string.toCharArray();
+            int count;
+            boolean doubloon = false;
+            for (int i = 0; i < c.length; i++) {
+                count = 0;
+                char u = c[i];
+                for (int j = 0; j < c.length; j++) {
+                    if (u == c[j]) {
+                        count++;
+                    }
+                }
+                if (count == 2) {
+                    doubloon = true;
+                } else {
+                    doubloon = false;
+                    break;
+                }
+            }
+            if (doubloon)
+                System.out.println(string + " is a doubloon");
+            else
+                System.out.println(string + " is not a doubloon");
+        }
+    }
+}
