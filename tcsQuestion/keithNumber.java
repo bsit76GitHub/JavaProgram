e30bd26d98912ed26e81363cb1ca3128113be7ee
bsit76GitHub <brajendrasharmait76@gmail.com>
2025-07-30 05:59:23 +0530

    adding wrapitup file

--- tcsQuestion/keithNumber.java
+++ tcsQuestion/keithNumber.java
@@ -6,28 +6,28 @@ import java.util.Scanner;
 public class keithNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        int digit = 0;
-        for (int i = num; i > 0; i = i / 10) {
-            digit++;
-        }
-        int a = 1, b = 9, c = 7, newnum = 0;
-        ArrayList<Integer> al = new ArrayList<>();
-        al.add(1);
-        al.add(9);
-        al.add(7);
-        for (int i = 0; al.get(i) <= num; i++) {
-            if (digit == 1) {
-                if (num ==1||num==9||num==7) {
-                    System.out.println(num + " is a Keith number");
-                    break;
-                }else {
-                    if (digit == 2) {
+        // int num = scanner.nextInt();
+        // int digit = 0;
+        // for (int i = num; i > 0; i = i / 10) {
+        //     digit++;
+        // }
+        // int a = 1, b = 9, c = 7, newnum = 0;
+        // ArrayList<Integer> al = new ArrayList<>();
+        // al.add(1);
+        // al.add(9);
+        // al.add(7);
+        // for (int i = 0; al.get(i) <= num; i++) {
+        //     if (digit == 1) {
+        //         if (num ==1||num==9||num==7) {
+        //             System.out.println(num + " is a Keith number");
+        //             break;
+        //         }else {
+        //             if (digit == 2) {
                         
-                    }
-                }
-            }
-        }
+        //             }
+        //         }
+        //     }
+        // }
         System.out.println("pending...");
         
     }
