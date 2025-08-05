c7b0ff49c76fa5f7f8df6de7a049c705c451cc3e
bsit76GitHub <brajendrasharmait76@gmail.com>
2025-08-05 22:38:30 +0530

    updating isAnnagram

--- tcsQuestion/week2/isAnagram.java
+++ tcsQuestion/week2/isAnagram.java
@@ -14,14 +14,14 @@ public class isAnagram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String[] str = new String[2*n];
+        String[] str = new String[2 * n];
         for (int i = 0; i < str.length; i++) {
             str[i] = sc.next();
         }
+        sc.close();
         for (int i = 0; i <= str.length / 2; i += 2) {
             String word1 = str[i];
             String word2 = str[i + 1];
-            
             boolean b = false;
             for (int j = 0; j < word1.length(); j++) {
                 for (int j2 = 0; j2 < word2.length(); j2++) {
